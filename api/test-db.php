de9b859c8c03292292b32ed1fa7cafa377850e70
Keshav Raj <keshavraj936@users.noreply.github.com>
2026-04-01 01:24:23 +0530

    Add DB fetch test

--- api/test-db.php
+++ api/test-db.php
@@ -1,4 +1,8 @@
 <?php
 require_once("../config/db.php");
 
-echo "DB Connected Successfully";
+$stmt = $conn->query("SELECT * FROM users");
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+header('Content-Type: application/json');
+echo json_encode($data);
