f9556460cded68378f53591c6fdee2355ea8707c
Keshav Raj <keshavraj936@users.noreply.github.com>
2026-04-06 04:18:14 +0530

    enable error debug

--- test-db.php
+++ test-db.php
@@ -1,5 +1,7 @@
 <?php
-require_once("../config/db.php");
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once("config/db.php");
 
 $stmt = $conn->query("SELECT * FROM users");
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
